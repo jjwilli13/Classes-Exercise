90fb70cc19e1fb6bee1b4facfc655dc0473c95e7
Jordan Williams <jjwilli13@gmail.com>
2023-03-05 07:01:58 -0600

    completed exercise

---
+++ Classes/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Classes
+{
+    public class Car
+    {
+        public Car()
+        { 
+
+        }
+
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        
+        
+    }
+        
+    
+        
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+
+            myCar.Make = "Toyota";
+            myCar.Model = "Sequoia";
+            myCar.Year = 2012;
+
+            Car secondCar = new Car();
+
+            secondCar.Make = "Chevy";
+            secondCar.Model = "Cavalier";
+            secondCar.Year = 2005;
+
+            Console.WriteLine($"{myCar.Make} {myCar.Model} {myCar.Year}");
+            Console.WriteLine($"{secondCar.Make} {secondCar.Model} {secondCar.Year}");
+
+            var carList = new List<Car>() { myCar, secondCar };
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
         }
     }
 }
